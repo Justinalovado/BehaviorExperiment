ee4a136216ec1e054114b6843b93f4993288dfa5
EngulfedMusic91 <osca5518@naver.com>
2022-12-04 06:48:58 +0000

    feature(front): install Alef font

--- bef/src/App.js
+++ bef/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const questions = ["What is the activity"]
+  const [question, setQuestion] = useState("What is the activity?");
   return (
     <div className="App">
-      <h1>Hello Sean</h1>
+      <h1>{question}</h1>
     </div>
   );
 }
