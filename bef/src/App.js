653c9e03ed441fa395ff2a5b70cc57e2b73b50fd
EngulfedMusic91 <osca5518@naver.com>
2022-12-04 12:10:50 +0000

    feature(front): add next,cancel buttons and adjust margin

--- bef/src/App.js
+++ bef/src/App.js
@@ -13,9 +13,13 @@ function App() {
         <Option />
         <Option />
         <Option />
-        <Option />
+        <Button className="addButton" text={"Add New +"} />
       </div>
-      <Button className="addButton" text={"Add New +"} />
+
+      <div className="button-container">
+        <Button className="nextButton" text={"Next ->"} />
+        <Button className="cancelButton" text={"Cancel"} />
+      </div>
     </div>
   );
 }
