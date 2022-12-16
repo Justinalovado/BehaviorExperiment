ae23d7d15b90ad8a824065926e9bcb49e6e7e006
Justinalovado <justinalovado@outlook.com>
2022-12-16 11:56:57 +1100

    build(front):added basic components in login form

--- bef/src/components/LoginForm.js
+++ bef/src/components/LoginForm.js
@@ -5,10 +5,18 @@ function LoginForm({showLogin, setShowLogin}) {
     const handleClose = () => {
         setShowLogin(!showLogin)
     }
+		const handleSubmit = () => {
+			console.log("make user login");
+		}
     return (
       <div className={`overlay-${showLogin?"show":"hide"}`}>
           <form className="form-container">
-              <input type="button" name="Close"onClick={handleClose}/>
+							<label for="email">Email:</label>
+							<input type="text" name="email" id="email"/>
+							<label for="psw">Password:</label>
+							<input type="password" name="psw" id="psw"/>
+							<input type="submit" className='login-submit-btn' name="Login" value="Login" onClick={handleSubmit}/>
+              <input type="button" className='close-btn' name="Close" value="Cancel" onClick={handleClose}/>
           </form>
       </div>
     )
