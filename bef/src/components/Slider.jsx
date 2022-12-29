556af555013c04c403dfcbe0ac193bf5b42d4944
Justinalovado <justinalovado@outlook.com>
2022-12-29 14:50:46 +1100

    feature(front):refined range slider css

--- bef/src/components/Slider.jsx
+++ bef/src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import "./Slider.css"
+
 
 function Slider( { curMetric, Metrics, setMetrics } ) {
 	const [slideValue, setslideValue] = useState(50);
@@ -10,6 +12,8 @@ function Slider( { curMetric, Metrics, setMetrics } ) {
 			...Metrics,
 			[curMetric]:val
 		})
+		const elem = document.getElementById(`${curMetric}-pointer`);
+		elem.style.paddingLeft = (val*0.942) + "%";
 	}
 
 	useEffect(() => {
@@ -19,11 +23,15 @@ function Slider( { curMetric, Metrics, setMetrics } ) {
 
   return (
 		<div className='slider'>
-			<div className='slide-value'>{slideValue}</div>
-			<input id={curMetric} type="range" min="0" max="100" step="10" onChange={handleUpdate}/>
-			<div>
-				<div className='range-min'>0</div>
-				<div className='range-max'>100</div>
+			<div className='pointer-track'id={`${curMetric}-pointer`}>
+				<div className='pointer-box'>
+					{slideValue}
+				</div>
+			</div>
+			<input className="slider-track" id={curMetric} type="range" min="0" max="100" step="5" onChange={handleUpdate}/>
+			<div className="label-container">
+				<div>0</div>
+				<div>100</div>
 			</div>
 		</div>
   )
--- bef/src/pages/PreActivity.js
+++ bef/src/pages/PreActivity.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Slider from '../components/Slider';
 import "./PreActivity.css";
 
 function PreActivity() {
 	const [Metrics, setMetrics] = useState({});
+	const [Part, setPart] = useState(0)
 
-	// const addMetric = (name, val) => {
-	// 	setMetrics(...Metrics, ...{[name]:[val]});
-	// }
 	const printMetric = () => {
 		console.log(Metrics);
 	}
 
-	
+	const nextSection = () => {
+		if (Part + 1 < section.length){
+			setPart(Part + 1);
+		}
+	}
+	const prevSection = () => {
+		if (Part > 0){
+			setPart(Part - 1);
+		}
+	}
+
+	const section = [
+		(
+			<div>
+				<div>Q1</div>
+				<Slider curMetric="met-1" Metrics={Metrics} setMetrics={setMetrics} />
+			</div>
+		),(
+			<div>
+				<div>Q2</div>
+				<Slider curMetric="met-2" Metrics={Metrics} setMetrics={setMetrics} />
+				<Slider curMetric="met-3" Metrics={Metrics} setMetrics={setMetrics} />
+			</div>
+		),
+
+	]
+
   return (
     <div className='container'>
 			<div className='slide-container'>
-				<Slider curMetric="met-1" Metrics={Metrics} setMetrics={setMetrics} />
-				<Slider curMetric="met-2" Metrics={Metrics} setMetrics={setMetrics} />
-				<button onClick={printMetric}>print</button>
+				{section[Part]}
+				{/* <button onClick={printMetric}>print</button> */}
 			</div>
 			<div className='btn-conatiner'>
-				buttons here
+				<button onClick={prevSection}>prev</button><br></br>
+				<button onClick={nextSection}>next</button>
 			</div>
     </div>
   )
