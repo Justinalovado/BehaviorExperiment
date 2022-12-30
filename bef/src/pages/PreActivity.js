c7a7ffe47e54bcf8630f27469cf5ab9e69c8d773
Justinalovado <justinalovado@outlook.com>
2022-12-30 22:44:42 +1100

    feature(front):bridged pages, restyled pre-act

--- bef/src/pages/PreActivity.js
+++ bef/src/pages/PreActivity.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Slider from '../components/Slider';
 import "./PreActivity.css";
 
 function PreActivity() {
 	const [Metrics, setMetrics] = useState({});
 	const [Part, setPart] = useState(0)
+	const navigate = useNavigate();
+	const NEXT_TXT = "Next ->";
+	const PREV_TXT = "<- Prev";
+
 
 	const printMetric = () => {
 		console.log(Metrics);
@@ -14,36 +19,40 @@ function PreActivity() {
 
 	const nextSection = () => {
 		if (Part + 1 < section.length){
-			setPart(Part + 1);
+			setPart(Part + 1)
+		}else{
+			navigate("/404")
 		}
 	}
 	const prevSection = () => {
 		if (Part > 0){
 			setPart(Part - 1);
+		}else{
+			navigate('/');
 		}
 	}
 
 	const section = [
 		(
 			<div className='met-questions'>
-				<div>How anxious are you?</div>
+				<h2>How anxious are you?</h2>
 				<Slider curMetric="met-1" Metrics={Metrics} setMetrics={setMetrics} />
 			</div>
 		),(
 			<div className='met-questions'>
-				<div>How strongly do you feel:</div>
-				<div>Your worst fear will happen?</div>
+				<h2>How strongly do you feel:</h2>
+				<p>Your worst fear will happen?</p>
 				<Slider curMetric="met-2" Metrics={Metrics} setMetrics={setMetrics} />
-				<div>You will be judged negatively?</div>
+				<p>You will be judged negatively?</p>
 				<Slider curMetric="met-3" Metrics={Metrics} setMetrics={setMetrics} />
-				<div>You will make a bad impression during the activity?</div>
+				<p>You will make a bad impression during the activity?</p>
 				<Slider curMetric="met-4" Metrics={Metrics} setMetrics={setMetrics} />
-				<div>You will appear anxious during the activity?</div>
+				<p>You will appear anxious during the activity?</p>
 				<Slider curMetric="met-5" Metrics={Metrics} setMetrics={setMetrics} />
 			</div>
 		),(
 			<div className='met-questions'>
-				<div>How bad do you think the outcome of the activity will be?</div>
+				<h2>How bad do you think the outcome of the activity will be?</h2>
 				<Slider curMetric="met-6" Metrics={Metrics} setMetrics={setMetrics} />
 			</div>
 		)
@@ -51,14 +60,14 @@ function PreActivity() {
 	]
 
   return (
-    <div className='container'>
+    <div className='pre-act-container'>
 			<div className='slide-container'>
 				{section[Part]}
 				{/* <button onClick={printMetric}>print</button> */}
 			</div>
-			<div className='btn-conatiner'>
-				<button onClick={prevSection}>prev</button><br></br>
-				<button onClick={nextSection}>next</button>
+			<div className='btn-container'>
+				<button onClick={prevSection}>{PREV_TXT}</button>
+				<button onClick={nextSection}>{NEXT_TXT}</button>
 			</div>
     </div>
   )
