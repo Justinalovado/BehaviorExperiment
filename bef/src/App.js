e154ef759459179691eee41869d5f7756c42db5f
EngulfedMusic91 <osca5518@naver.com>
2023-01-07 10:21:59 +0900

    feature(front): add background color

--- bef/src/App.js
+++ bef/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import EventPlanning from './pages/EventPlanning'
+import PostActivity from './pages/PostActivity'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-        <EventPlanning />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<EventPlanning />} />
+        <Route path="/PostActivity" element={<PostActivity />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ bef/src/pages/PostActivity.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './PostActivity.css'
+
+function PostActivity() {
+    return (
+        <div className="PostActivity">
+            
+            <Link to="/">Back to Event Planning</Link>
+        </div>
+  )
+}
+
+export default PostActivity
