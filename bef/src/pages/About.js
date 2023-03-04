799e4e3cb498cff3aaeff94cd271b3365ec09c37
EngulfedMusic91 <osca5518@naver.com>
2023-03-04 14:28:00 +0000

    fix(front): fix classname spelling

--- bef/src/pages/About.js
+++ bef/src/pages/About.js
@@ -10,7 +10,7 @@ function About() {
   return (
     <div className="About-page">
       <h2 className="section-title fadeInDown">Welcome to BEF</h2>
-      <p className="section-subtitle fa[deInDown">Designed to help patients track their progress and share their experiences with their psychologists during Cognitive Behavioural Therapy (CBT).</p>
+      <p className="section-subtitle fadeInDown">Designed to help patients track their progress and share their experiences with their psychologists during Cognitive Behavioural Therapy (CBT).</p>
       <p className="fadeInDown">With our easy-to-use interface, patients can easily record their thoughts, feelings, and behaviours over time, giving them a clear picture of their progress and helping them stay motivated on their journey to better mental health.</p>
       <p className="fadeInDown">By providing valuable insights into patients' experiences, our BEF also helps psychologists tailor their treatment plans to better suit their patients' needs, ultimately leading to more successful outcomes.</p>
       {/* <button className="backToMenuButton" onClick={warpHome}>Back To Home</button> */}
